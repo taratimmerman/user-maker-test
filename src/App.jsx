db8b1c3331704740d0e8b4081dcafa2021c0d1d0
taratimmerman <timmermantara@gmail.com>
2021-10-04 18:01:04 -0600

    create main component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => (
+  <main>
+    <section>
+      <h1>Make a User</h1>
+    </section>
+  </main>
+);
+
+export default Main;
