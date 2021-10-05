30b633cbd7bc01e768c6be2753f775c91b6dfbb9
taratimmerman <timmermantara@gmail.com>
2021-10-04 18:26:00 -0600

    create database component

---
+++ src/components/Database.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Database = () => (
+  <section>
+    <h1>Database Users</h1>
+  </section>
+);
+
+export default Database;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Form from './Form';
+import Database from './Database';
 
 const Main = () => (
   <main>
     <Form />
+    <Database />
   </main>
 );
 
